443198a12733f91d581d731a1b28c9eb9af42581
umblox <39423646+umblox@users.noreply.github.com>
2024-07-31 08:07:39 +0700

    Create telebot.sh

---
+++ telebot.sh
@@ -0,0 +1,20 @@
+#!/bin/sh /etc/rc.common
+# Copyright (C) 2024 Maizil41
+
+START=99
+
+start() {
+    echo "Starting BOT"
+    /usr/bin/telebot > /var/log/telebot.log 2>&1 &
+}
+
+stop() {
+    echo "Stopping BOT"
+    killall telebot
+}
+
+restart() {
+    stop
+    sleep 1
+    start
+}
